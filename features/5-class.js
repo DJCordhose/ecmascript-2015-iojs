bc66060949e17a444d34b8849cd114d3c9b3a044
Nils Hartmann <nils@nilshartmann.net>
2015-05-22 09:38:41 +0200

    Initial draft class example

--- features/5-class.js
+++ features/5-class.js
@@ -1,3 +1,88 @@
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/class
 
-TODO
+'use strict';
+// 
+// Use 'class' to define classes...
+class Person {
+    constructor(name) {
+        this.name = name;
+		this._id = name.toLowerCase();
+    }
+	
+	// read only property, note that the setter is missing
+	get id() {
+		return this._id;
+	}
+	
+	// Example: Use getter and setter to 'intercept' setting/getting properties
+	set password(password) {
+		// store 'encrypted' password
+		this.encryptedPassword = password.split('').reverse().join('');
+	}
+	
+	get password() {
+		// return 'decrypted' password
+		return this.encryptedPassword.split('').reverse().join('');
+	}
+
+    toString() {
+        return `I'm ${this.name}`;
+    }
+}
+
+const lemmy = new Person('Lemmy');
+console.log(lemmy.toString()); // I'm Lemmy
+console.log(lemmy.id); // 'lemmy'
+try {
+	// Won't work, as birthday is a read-only property
+	lemmy.id  = 'kilmister';
+} catch (e) {
+	// ERROR: TypeError: Cannot set property id of [object Object] which has only a getter
+	console.log(e);
+}
+
+lemmy.password='aceOfSpades';
+console.log(lemmy.password); // aceOfSpades
+console.log(lemmy.encryptedPassword); //sedapSfOeca
+
+
+// You can easily create subclasses using the `extends` keyword:
+class Musician extends Person {
+	constructor(name, instrument) {
+		// invoke super constructor first
+		super(name);
+		this.instrument = instrument;
+	}
+	
+	toString() {
+		return `${super.toString()} and I'm playing ${this.instrument}`;
+	}
+	
+	// You can define static methods
+	static newGuitarPlayer(name) {
+		return new Musician(name, 'Guitar');
+	}
+}
+
+const mikkey = new Musician('Mikkey', 'Drums');
+console.log(mikkey.toString()); // I'm Mikkey and I'm playing Drums
+console.log((mikkey instanceof Person)); // true
+console.log((mikkey instanceof Musician)); // true
+console.log(Musician.newGuitarPlayer('Phil').toString()); // I'm Phil and I'm playing Guitar
+
+// And like in the enhanced object literal you can even use computed property names using the square bracket notation:
+function secretKey() {
+	return 667;
+}
+
+class NeighbourHood {
+	[ 'no_' + secretKey() ]() {
+		return 'Neighbour of the beast';
+	}
+}
+
+const hood = new NeighbourHood();
+console.log(hood.no_667()); // Neighbour of the beast
+
+
+	
